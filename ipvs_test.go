86c3f3b11abe7e92e4eb1cff18098076f1e83f9b
Davanum Srinivas <davanum@gmail.com>
2020-03-11 19:48:34 -0400

    Bump dependencies
    - newer github.com/pkg/errors (0.9.1)
    - tiny change in github.com/sirupsen/logrus (1.4.2)
    - minor change in github.com/vishvananda/netlink (1.1.0)
    - github.com/vishvananda/netns stayed the same (no updates in repo)
    - golang.org/x/sys (match what's in containerd/containerd)
    - gotest.tools (switch to v3)
    
    Signed-Off-By: Davanum Srinivas <davanum@gmail.com>

--- ipvs_test.go
+++ ipvs_test.go
@@ -11,8 +11,8 @@ import (
 	"github.com/vishvananda/netlink"
 	"github.com/vishvananda/netlink/nl"
 	"golang.org/x/sys/unix"
-	"gotest.tools/assert"
-	is "gotest.tools/assert/cmp"
+	"gotest.tools/v3/assert"
+	is "gotest.tools/v3/assert/cmp"
 )
 
 var (
